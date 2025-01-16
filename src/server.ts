56dd0956b03926cabbb54a5f2657ad897a53f396
Ronald Camargo <ronaldcamargodev@gmail.com>
2025-01-15 21:38:46 -0400

    aplicando zod

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const transactions = await knex('transactions')
+  return transactions
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running on port 3333')
 })
